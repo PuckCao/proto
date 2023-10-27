e4df2b1b162223746509370b1865a897705a9a6c
puck <caoyuan@miliantech.com>
2023-10-27 22:26:44 +0800

    phone验证码

--- user/user.pb.go
+++ user/user.pb.go
@@ -165,112 +165,29 @@ func (m *ErrMsg) GetMsg() string {
 	return ""
 }
 
-type PhoneCheckReq struct {
-	Phone                string   `protobuf:"bytes,1,opt,name=phone,proto3" json:"phone,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *PhoneCheckReq) Reset()         { *m = PhoneCheckReq{} }
-func (m *PhoneCheckReq) String() string { return proto.CompactTextString(m) }
-func (*PhoneCheckReq) ProtoMessage()    {}
-func (*PhoneCheckReq) Descriptor() ([]byte, []int) {
-	return fileDescriptor_ed89022014131a74, []int{3}
-}
-
-func (m *PhoneCheckReq) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_PhoneCheckReq.Unmarshal(m, b)
-}
-func (m *PhoneCheckReq) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_PhoneCheckReq.Marshal(b, m, deterministic)
-}
-func (m *PhoneCheckReq) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PhoneCheckReq.Merge(m, src)
-}
-func (m *PhoneCheckReq) XXX_Size() int {
-	return xxx_messageInfo_PhoneCheckReq.Size(m)
-}
-func (m *PhoneCheckReq) XXX_DiscardUnknown() {
-	xxx_messageInfo_PhoneCheckReq.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_PhoneCheckReq proto.InternalMessageInfo
-
-func (m *PhoneCheckReq) GetPhone() string {
-	if m != nil {
-		return m.Phone
-	}
-	return ""
-}
-
-type PhoneCheckResp struct {
-	ErrMsg               *ErrMsg  `protobuf:"bytes,1,opt,name=errMsg,proto3" json:"errMsg,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *PhoneCheckResp) Reset()         { *m = PhoneCheckResp{} }
-func (m *PhoneCheckResp) String() string { return proto.CompactTextString(m) }
-func (*PhoneCheckResp) ProtoMessage()    {}
-func (*PhoneCheckResp) Descriptor() ([]byte, []int) {
-	return fileDescriptor_ed89022014131a74, []int{4}
-}
-
-func (m *PhoneCheckResp) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_PhoneCheckResp.Unmarshal(m, b)
-}
-func (m *PhoneCheckResp) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_PhoneCheckResp.Marshal(b, m, deterministic)
-}
-func (m *PhoneCheckResp) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_PhoneCheckResp.Merge(m, src)
-}
-func (m *PhoneCheckResp) XXX_Size() int {
-	return xxx_messageInfo_PhoneCheckResp.Size(m)
-}
-func (m *PhoneCheckResp) XXX_DiscardUnknown() {
-	xxx_messageInfo_PhoneCheckResp.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_PhoneCheckResp proto.InternalMessageInfo
-
-func (m *PhoneCheckResp) GetErrMsg() *ErrMsg {
-	if m != nil {
-		return m.ErrMsg
-	}
-	return nil
-}
-
 func init() {
 	proto.RegisterType((*RegisterReq)(nil), "user.RegisterReq")
 	proto.RegisterType((*RegisterResp)(nil), "user.RegisterResp")
 	proto.RegisterType((*ErrMsg)(nil), "user.ErrMsg")
-	proto.RegisterType((*PhoneCheckReq)(nil), "user.PhoneCheckReq")
-	proto.RegisterType((*PhoneCheckResp)(nil), "user.PhoneCheckResp")
 }
 
 func init() { proto.RegisterFile("user/user.proto", fileDescriptor_ed89022014131a74) }
 
 var fileDescriptor_ed89022014131a74 = []byte{
-	// 244 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x91, 0x4f, 0x4b, 0xc4, 0x40,
-	0x0c, 0xc5, 0xa9, 0xad, 0x65, 0xcd, 0xae, 0xff, 0xe2, 0x1e, 0x4a, 0x4f, 0x32, 0x28, 0x78, 0xaa,
-	0xb8, 0x8a, 0x7e, 0x00, 0xf1, 0x28, 0xc8, 0x80, 0x27, 0x4f, 0xeb, 0x6e, 0x68, 0x45, 0xda, 0x19,
-	0x93, 0x8a, 0x5f, 0x5f, 0x66, 0xa6, 0xff, 0xd4, 0x8b, 0x97, 0x92, 0xbc, 0xf7, 0x48, 0x7e, 0xe9,
-	0xc0, 0xe1, 0xa7, 0x10, 0x5f, 0xba, 0x4f, 0x61, 0xd9, 0xb4, 0x06, 0x13, 0x57, 0xab, 0x17, 0x98,
-	0x6b, 0x2a, 0xdf, 0xa4, 0x25, 0xd6, 0xf4, 0x81, 0x39, 0xcc, 0x9c, 0xdc, 0xac, 0x6b, 0xca, 0xa2,
-	0xd3, 0xe8, 0x62, 0x4f, 0x0f, 0xbd, 0xf3, 0xec, 0x5a, 0xe4, 0xcb, 0xf0, 0x36, 0xdb, 0x09, 0x5e,
-	0xdf, 0xe3, 0x12, 0x76, 0x6d, 0x65, 0x1a, 0xca, 0x62, 0x6f, 0x84, 0x46, 0xdd, 0xc0, 0x62, 0x1c,
-	0x2e, 0x16, 0xcf, 0x20, 0x25, 0xe6, 0x47, 0x29, 0xfd, 0xec, 0xf9, 0x6a, 0x51, 0x78, 0x9e, 0x07,
-	0xaf, 0xe9, 0xce, 0x53, 0x05, 0xa4, 0x41, 0x41, 0x84, 0x64, 0x63, 0xb6, 0x81, 0x24, 0xd6, 0xbe,
-	0xc6, 0x23, 0x88, 0x6b, 0x29, 0x3b, 0x00, 0x57, 0xaa, 0x73, 0xd8, 0x7f, 0x72, 0xeb, 0xee, 0x2b,
-	0xda, 0xbc, 0xbb, 0x23, 0x06, 0x98, 0x68, 0x0a, 0x73, 0x0b, 0x07, 0xd3, 0xd8, 0x7f, 0x71, 0x56,
-	0x0c, 0xc9, 0xb3, 0x10, 0xe3, 0x15, 0xcc, 0xfa, 0x63, 0xf0, 0x38, 0x24, 0x27, 0x7f, 0x2e, 0xc7,
-	0xdf, 0x92, 0x58, 0xbc, 0x03, 0x18, 0x57, 0xe2, 0x49, 0x48, 0xfc, 0x60, 0xcd, 0x97, 0x7f, 0x45,
-	0xb1, 0xaf, 0xa9, 0x7f, 0xa2, 0xeb, 0xef, 0x00, 0x00, 0x00, 0xff, 0xff, 0x73, 0x36, 0xf2, 0x51,
-	0xb5, 0x01, 0x00, 0x00,
+	// 200 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2f, 0x2d, 0x4e, 0x2d,
+	0xd2, 0x07, 0x11, 0x7a, 0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0x2c, 0x20, 0xb6, 0x52, 0x34, 0x17,
+	0x77, 0x50, 0x6a, 0x7a, 0x66, 0x71, 0x49, 0x6a, 0x51, 0x50, 0x6a, 0xa1, 0x90, 0x14, 0x17, 0x07,
+	0x48, 0x38, 0x2f, 0x31, 0x37, 0x55, 0x82, 0x51, 0x81, 0x51, 0x83, 0x33, 0x08, 0xce, 0x07, 0xc9,
+	0x15, 0x24, 0x16, 0x17, 0x97, 0xe7, 0x17, 0xa5, 0x48, 0x30, 0x41, 0xe4, 0x60, 0x7c, 0x21, 0x11,
+	0x2e, 0xd6, 0x82, 0x8c, 0xfc, 0xbc, 0x54, 0x09, 0x66, 0xb0, 0x04, 0x84, 0xa3, 0x64, 0xc2, 0xc5,
+	0x83, 0x30, 0xbc, 0xb8, 0x40, 0x48, 0x85, 0x8b, 0x2d, 0xb5, 0xa8, 0xc8, 0xb7, 0x38, 0x1d, 0x6c,
+	0x36, 0xb7, 0x11, 0x8f, 0x1e, 0xd8, 0x3d, 0xae, 0x60, 0xb1, 0x20, 0xa8, 0x9c, 0x92, 0x1e, 0x17,
+	0x1b, 0x44, 0x44, 0x48, 0x88, 0x8b, 0x25, 0x39, 0x3f, 0x05, 0xe2, 0x12, 0xe6, 0x20, 0x30, 0x5b,
+	0x48, 0x80, 0x8b, 0x39, 0xb7, 0x38, 0x1d, 0xea, 0x00, 0x10, 0xd3, 0xc8, 0x92, 0x8b, 0x25, 0xb4,
+	0x38, 0xb5, 0x48, 0xc8, 0x90, 0x8b, 0x03, 0x66, 0x9b, 0x90, 0x20, 0xc4, 0x64, 0x24, 0xaf, 0x49,
+	0x09, 0xa1, 0x0b, 0x15, 0x17, 0x24, 0xb1, 0x81, 0x83, 0xc2, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff,
+	0x0c, 0x4c, 0x92, 0x30, 0x1d, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -286,7 +203,6 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type UserClient interface {
 	Register(ctx context.Context, in *RegisterReq, opts ...grpc.CallOption) (*RegisterResp, error)
-	PhoneCheck(ctx context.Context, in *PhoneCheckReq, opts ...grpc.CallOption) (*PhoneCheckResp, error)
 }
 
 type userClient struct {
@@ -306,19 +222,9 @@ func (c *userClient) Register(ctx context.Context, in *RegisterReq, opts ...grpc
 	return out, nil
 }
 
-func (c *userClient) PhoneCheck(ctx context.Context, in *PhoneCheckReq, opts ...grpc.CallOption) (*PhoneCheckResp, error) {
-	out := new(PhoneCheckResp)
-	err := c.cc.Invoke(ctx, "/user.User/PhoneCheck", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // UserServer is the server API for User service.
 type UserServer interface {
 	Register(context.Context, *RegisterReq) (*RegisterResp, error)
-	PhoneCheck(context.Context, *PhoneCheckReq) (*PhoneCheckResp, error)
 }
 
 // UnimplementedUserServer can be embedded to have forward compatible implementations.
@@ -327,9 +233,6 @@ type UnimplementedUserServer struct {
 
 func (*UnimplementedUserServer) Register(ctx context.Context, req *RegisterReq) (*RegisterResp, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Register not implemented")
-}
-func (*UnimplementedUserServer) PhoneCheck(ctx context.Context, req *PhoneCheckReq) (*PhoneCheckResp, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method PhoneCheck not implemented")
 }
 
 func RegisterUserServer(s *grpc.Server, srv UserServer) {
@@ -354,24 +257,6 @@ func _User_Register_Handler(srv interface{}, ctx context.Context, dec func(inter
 	return interceptor(ctx, in, info, handler)
 }
 
-func _User_PhoneCheck_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(PhoneCheckReq)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(UserServer).PhoneCheck(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/user.User/PhoneCheck",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServer).PhoneCheck(ctx, req.(*PhoneCheckReq))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 var _User_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "user.User",
 	HandlerType: (*UserServer)(nil),
@@ -379,10 +264,6 @@ var _User_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Register",
 			Handler:    _User_Register_Handler,
-		},
-		{
-			MethodName: "PhoneCheck",
-			Handler:    _User_PhoneCheck_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
